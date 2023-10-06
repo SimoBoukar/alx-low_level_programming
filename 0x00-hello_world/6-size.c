228d4e23c98a9d9a44a73c80187b4e6b5d64fe4d
Mohammed <6bouass9@gmail.com>
2023-10-05 18:26:44 -0700

    me

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+*Description: a C program that print sizeof various types
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of a", sizeof(char));
+printf("Size of an", sizeof(int));
+printf("Size of a", sizeof(long));
+printf("Size of a", sizeof(long long));
+printf("Size of a", sizeof(float));
+return (0);
+}
