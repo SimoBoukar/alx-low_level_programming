7b9e34039cb9b37744b36b60044a8ffbb287e0a0
Mohammed <6bouass9@gmail.com>
2023-10-05 18:54:40 -0700

    me

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Description: a C program that print a quote using write function
+*
+* Return: 1 (Not success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
