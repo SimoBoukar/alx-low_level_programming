46aa60e75daede6a0d9b407e2e45cac78f58d9a2
Mohammed <6bouass9@gmail.com>
2023-10-23 14:58:55 -0700

    1st commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    for (i = 0; n > 0; i++, n--)
-    {
-        s[i] = b;
-    }
-    return (s);
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
