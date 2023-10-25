f2218c2a6d6dfee881f9aa45925ee242765d8a6e
Mohammed <6bouass9@gmail.com>
2023-10-25 14:43:15 -0700

    5th commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,8 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: int
  * @y: int
- * Return: -1 if y lower than 0, x if y equal 1, 1 if y equal 0, else print x aised to the power of y
+ * Return: -1 if y lower than 0, x if y equal 1, 1 if y equal 0,
+ * else print x aised to the power of y
 */
 
 int _pow_recursion(int x, int y)
