1a996bfa0c4605b6ed6a33b8274f54a867ccba44
Mohammed <6bouass9@gmail.com>
2023-10-31 16:47:27 -0700

    3rd commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: string
+ * Return: int
+*/
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0' ; size++)
+	;
+	return (size);
+}
+
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size1, size2, n;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc((size1 + size2) * sizeof(char) + 1);
+
+	if (ptr == 0)
+		return (0);
+
+	for (n = 0; n <= size1 + size2; n++)
+	{
+		if (n <= size1)
+			ptr[n] = s1[n];
+		else
+			ptr[n] = s2[n - size1];
+	}
+		ptr[n] = '\0';
+		return (ptr);
+}
