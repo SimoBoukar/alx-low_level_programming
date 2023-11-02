2e22450a0a4c90dcb7407c32d11dc719907ef1c5
Mohammed <6bouass9@gmail.com>
2023-11-02 14:25:54 -0700

    1st commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: int
+ * Return: pointer to an array
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == 0)
+		exit(98);
+
+	return (ptr);
+}
