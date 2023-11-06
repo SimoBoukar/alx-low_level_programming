92b570c496760ba1c5d7e89a55ff8b62cd32888f
Mohammed <6bouass9@gmail.com>
2023-11-06 08:16:01 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
-	char* name;
+/**
+ * struct dog - structure
+ * @name: pointer of type char
+ * @age: float
+ * @owner: pointer of type char
+*/
+
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif /* _DOG_H */
