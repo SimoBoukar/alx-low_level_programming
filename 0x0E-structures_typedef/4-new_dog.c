583402c08183014ff9790a8c65ad8eb1a59e1df4
Mohammed <6bouass9@gmail.com>
2023-11-06 16:53:11 -0800

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,7 +30,7 @@ char *_strcpy(char *destination, char *source)
 
 	for (a = 0; source[a]; a++)
 		destination[a] = source[a];
-		destination[a] = '\0';
+	destination[a] = '\0';
 
 		return (destination);
 }
@@ -54,14 +54,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	(*dog).name = malloc(sizeof(char) * (_strlen(name) + 1);
+	(*dog).name = malloc(sizeof(char) * (_strlen(name) + 1));
 			if ((*dog).name == NULL)
 			{
 			free((*dog).name);
 			free(dog);
 			return (NULL);
 			}
-	(*dog).owner = malloc(sizeof(char) * (_strlen(owner) + 1);
+	(*dog).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 			if ((*dog).owner == NULL)
 			{
 			free((*dog).owner);
@@ -70,6 +70,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			}
 		(*dog).name = _strcpy((*dog).name, name);
 		(*dog).age = age;
-		(*dog).owner = _strcpy((*dog).owner);
-	return (dog);
+		(*dog).owner = _strcpy((*dog).owner, owner);
+
+			return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+char * _strcpy(char *destination, char *source);
+int _strlen(const char *string);
 
 #endif /* _DOG_H */
