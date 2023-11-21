97b83e617850a81256417ccf1f7bdd305ba44110
Mohammed <6bouass9@gmail.com>
2023-11-21 14:45:30 -0800

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,19 +14,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *node;
 
 	if (!head || !new_node)
-		return (NULL);
+	return (NULL);
 
 	new_node->next = NULL;
 	new_node->n = n;
-
 	if (!*head)
-	*head = *new_node;
+	*head = new_node;
 	else
 	{
 	node = *head;
 	while (node->next)
-		node = node->next;
-		node->next = new_node;
+	node = node->next;
+	node->next = new_node;
 	}
 	return (new_node);
 }
