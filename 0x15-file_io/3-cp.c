319464ea55257b901bc7e6c9440d4cc01c12df27
Mohammed <6bouass9@gmail.com>
2023-11-27 07:26:20 -0800

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,14 +28,14 @@ int main(int ac, char **av)
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-	while (b = read(from_fd, buf, READ_BUF_SIZE) > 0)
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
 	from_fd = close(from_fd);
-	to_fd = close(to_fd)
+	to_fd = close(to_fd);
 		if (from_fd)
 			dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 		if (to_fd)
