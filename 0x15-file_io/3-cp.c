2d162cf18060813589a1925df6e7e7f81c75e627
Mohammed <6bouass9@gmail.com>
2023-11-27 07:24:41 -0800

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,6 +28,9 @@ int main(int ac, char **av)
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	while (b = read(from_fd, buf, READ_BUF_SIZE) > 0)
+		if (write(to_fd, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99)
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
