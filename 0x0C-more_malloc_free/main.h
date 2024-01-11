a00713eadc3ffb323cb2003940b5d8c8349df185
Mohammed <6bouass9@gmail.com>
2024-01-11 15:45:20 -0800

    Mandatory tasks

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<stddef.h>
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memset(char *s, char b, unsigned int n);
-void _puts(char *ptrs);
-int _atoi(const char *s);
-void print_int(unsigned long int a);
-int main(int argc, char const *argv[]);
-int _putchar(char c);
+
+
 #endif
--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,16 +12,16 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *node = NULL, *next = NULL;
 
 	if (!head || !*head)
-		return (NULL);
+	return (NULL);
 
 	node = *head;
 	*head = NULL;
 	while (node)
 	{
-		next = node->next;
-		node->next = *head;
-		*head = node;
-		node = next;
+	next = node->next;
+	node->next = *head;
+	*head = node;
+	node = next;
 	}
 	return (*head);
 }
