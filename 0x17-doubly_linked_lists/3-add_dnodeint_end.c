0c9049859a69ff7529945e0aa713dab5e9b1379e
Mohammed <6bouass9@gmail.com>
2024-01-11 16:07:19 -0800

    Mandatory tasks

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,7 +28,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (node->next)
 			node = node->next;
 
-		node->next = next;
+		node->next = new;
 		new->prev = node;
 	}
 	return (new);
