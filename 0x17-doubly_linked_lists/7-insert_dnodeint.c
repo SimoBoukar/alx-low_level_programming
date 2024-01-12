556a202781a5ce4fa958c128a53e8b4c8dc9cdbb
Mohammed <6bouass9@gmail.com>
2024-01-11 16:19:04 -0800

    Mandatory tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
